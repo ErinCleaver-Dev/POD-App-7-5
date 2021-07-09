6bbe5847a784c87b773f508d6fd592cc73bef4de
ecleaver <erincleaver@grcc.email.edu>
2021-07-08 23:34:53 -0400

    Update git

--- static/javascript/search.js
+++ static/javascript/search.js
@@ -1,26 +1,20 @@
 
 
 
-async function loadStudents() {
+async function loadCards() {
     let search_results = document.querySelector('.search-results');
+    let cardTemplate = Handlebars.compile(
+        document.getElementById('card-template').innerHTML);
     let resp = await fetch('https://erincleaver-grcc.github.io/songs/songs.json');
 
     console.log(resp);
 
     let result = await resp.json();
     console.log(result);
-
+    
     result['songs'].map(item => {
-
-        let formated_card = `<div class="card">
-        <div class="card-image" style="background-image: url('${item.img}');background-size: cover;background-position: center;">
-            
-        </div>
-        <p class='podcast_title'>${item.name}</p>
-        <p>${item.genre}</p>
-        <input type="submit" id="listen" class="button_rules" value="Listen">
-        </div>`
-        search_results.innerHTML += formated_card;
+        
+        search_results.innerHTML += cardTemplate(item);
     });
 
 
@@ -32,5 +26,5 @@ async function loadStudents() {
 }
 
 
-loadStudents();
+loadCards();
 
